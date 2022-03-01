c8aa71b97edd978f4d43ce674b1754ec840f08ef
raiansakil <aburaian182@gmail.com>
2022-03-01 16:11:49 +0600

    added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
 const searchButton = () => {
-    const searchPhone = document.getElementById('search-phone').value
+    document.getElementById("phone-area").innerHTML = ""
+    document.getElementById("details-container").innerHTML = ""
+    const searchPhone = document.getElementById("search-phone").value
+    document.getElementById("search-phone").value = ""
+    const error = document.getElementById("error")
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`
     fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhone(data.data))
-}
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.data == null);
+            if (data.data == 0 || searchPhone == "") {
+                error.innerText = "No Phone Found"
+                phoneArea.innerText = ""
 
+            } else {
+                displayPhone(data.data)
+                error.innerText = "";
+
+            }
+        })
+
+}
 const displayPhone = (phones) => {
     const phoneData20 = phones.slice(0, 20)
+    const phoneArea = document.getElementById("phone-area")
+    phoneArea.textContent = ""
     phoneData20.forEach(phone => {
         console.log(phone)
-        const phoneArea = document.getElementById('phone-area')
-        const div = document.createElement('div')
+        const div = document.createElement("div")
         div.classList.add("col-lg-4")
         div.classList.add("col-sm-12")
         div.classList.add("my-4")
@@ -29,10 +45,53 @@ const displayPhone = (phones) => {
         phoneArea.appendChild(div)
     });
 }
-const phoneDetails = (details) => {
+const phoneDetails = (slugId) => {
     // console.log(details)
-    const url = ` https://openapi.programming-hero.com/api/phone/${details}`
+    const url = ` https://openapi.programming-hero.com/api/phone/${slugId}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data.mainFeatures))
+        .then(data => displayDetails(data.data))
+}
+
+const displayDetails = (details) => {
+    console.log(details)
+    const detailsContainer = document.getElementById("details-container")
+    const div = document.createElement('div')
+    div.classList.add("col-lg-4")
+    div.classList.add("col-sm-12")
+    div.classList.add("my-4")
+    div.innerHTML = `
+             <img src = "${details.image}"
+             class = "img-thumnail"
+             alt = "..">
+            <h2 class = "my-2"> ${details.name} </h2>
+            <h3>${details.releaseDate}</h3>
+            <h4 class = "my-2">MainFeatures: </h4>
+            <ul class = "my-2">
+                <li>ChipSet:  ${details.mainFeatures.chipSet}</li>
+                <li>DisplaySize:  ${details.mainFeatures.displaySize}</li>
+                <li>Memory:  ${details.mainFeatures.memory}</li>
+                <li>Storage: ${details.mainFeatures.storage}</li>
+                <h4 class = "my-2"> Sensors: </h4>
+                <ul>
+                    <li> ${details.mainFeatures.sensors[0]}</li> 
+                    <li> ${details.mainFeatures.sensors[1]} </li> 
+                    <li> ${details.mainFeatures.sensors[2]} </li>
+                    <li> ${details.mainFeatures.sensors[3]} </li> 
+                    <li> ${details.mainFeatures.sensors[4]} </li> 
+                    <li> ${details.mainFeatures.sensors[5]} </li> 
+                    <li> ${details.mainFeatures.sensors[6]} </li> 
+                </ul>
+            </ul>
+            <h4 class = "my-2" > Others: </h4>
+            <ul>
+                <li>Bluetooth: ${details.others.Bluetooth} </li> 
+                <li>GPS: ${details.others.GPS} </li> 
+                <li>NFC: ${details.others.NFC} </li>
+                <li>Radio: ${details.others.Radio} </li> 
+                <li>USB: ${details.others.USB} </li> 
+                <li>WLAN: ${details.others.WLAN} </li>
+            </ul> 
+    `
+    detailsContainer.appendChild(div)
 }
